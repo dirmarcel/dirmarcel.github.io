c4768933a9732b5738fd12ccb1b42e9ef18956be
dirmarcel <marcepan.lawa576@gmial.com>
2022-04-14 19:50:24 +0200

    add: tables of content

--- js/gitMain.js
+++ js/gitMain.js
@@ -6,6 +6,31 @@ var log = (function () {
   }
 })();
 
+window.log = log;
+/**
+ * Main mounting point in global scope
+ */
+window.sasync = {
+  loaded: {},
+};
+
+//Error hadnling with body loading
+(function () {
+  let found;
+  try {
+    found = Boolean(document.body);
+  } catch (e) {}
+
+  if (!found) {
+    document.write(
+      `<h1 style="color: red; position: fixed; top: 30%; background-color: white;">Can't find document.body - it probably means that /github.js is loaded in &lt;head> instead at the end of &lt;body></h1>`
+    );
+  }
+})();
+
 //header implementation
 var header = document.querySelector("header");
 header.innerHTML = '<a href="/index.html">dirname.github.io</a>';
+//github-link implementation
+var githubProfile = document.querySelector("github-profile");
+githubLink.innerHTML = "";
